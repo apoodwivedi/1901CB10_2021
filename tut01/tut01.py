cd103cbcbb9cdf3dac226c36ab5112df72a3a8a7
apoorvadwivedi <apoorvadwivedi2001@gmail.com>
2021-08-18 21:12:54 +0530

    Assignment updated

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,1 +1,35 @@
-print("My name is Apoorva. My roll number is 1901CB10")
+# func to detect meraki number
+
+def meraki_helper(n):
+    lengthOfList = len(n)  # size of input list
+    countMeraki = 0  # counter for number of mearki numbers
+
+    # iterating through list of input
+    for i in range(lengthOfList):
+        meraki_yes = True
+        # type casting each number to string for easier navigation through digits
+        element = str(n[i])
+        lengthOfString = len(element)  # length of string
+
+        if(lengthOfString == 1):
+            meraki_yes = True
+            countMeraki += 1
+        else:
+            for i in range(0, lengthOfString-1):
+                # checking if the diff between adjacent digits is 1 by casting string back into integer
+                if(abs(int(element[i])-int(element[i+1])) == 1):
+                    meraki_yes = True
+                    countMeraki += 1
+                    break
+
+        ans = "Yes" if meraki_yes else "No"
+        x = " not" if meraki_yes != True else ""
+        print("{} - {} is{} a Meraki number".format(ans, element, x))
+
+    non_meraki = lengthOfList - countMeraki  # count of non-meraki numbers
+    print("The input list contains {} Meraki and {} Non-Meraki numbers".format(countMeraki, non_meraki))
+
+
+input = [12, 14, 56, 78, 98, 54, 678, 134,
+         789, 0, 7, 5, 123, 45, 76345, 987654321]
+meraki_helper(input)  # passing list into the function
